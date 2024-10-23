4cedf79ee5a8e87a0425a01e1881aa6b643f6b6e
Brayham Morales <moralesbrayham@gmail.com>
2024-10-23 17:12:05 -0600

    Add input from file

--- MySweetProgram/MySweetProgram.java
+++ MySweetProgram/MySweetProgram.java
@@ -1,13 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class MySweetProgram{
 
-    public static void main(String[] args) {
-        System.out.println("Enter your name");
-        
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("Students.txt"));
 
-        if(s.equals("Caleb")){
+        List<String> students = new ArrayList<String>();
+
+        while(in.hasNextLine()){
+            students.add(in.nextLine());
+        }
+        for(int i = 0; i < students.size(); i++){
+            System.out.println("name: " + students.get(i));
+        }
+
+        in.close();
+        /*Scanner in = new Scanner(System.in);
+        System.out.println("Give us a size");
+        int size = in.nextInt();
+        
+        int[] grades = new int[size];
+
+        System.out.println("Enter " + size + " numbers. Press enter after each");
+        for(int i = 0; i < size; i++){
+            grades[i] = in.nextInt();
+        }
+
+        for(int i = 0; i < size; i++){
+            System.out.println(grades[i]);
+        }
+
+        //String s = in.nextLine();
+
+        /*if(s.equals("Caleb")){
             System.out.println("Hey Caleb");
         }
         else if(s.equals("Sally")){
@@ -15,9 +43,9 @@ public class MySweetProgram{
         }
         else{
             System.out.println("You're not welcome here");
-        }
+        }*/
 
-        in.close();
+        //in.close();
 
         //loops
 
