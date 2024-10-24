04f8a4f3e58e2f89b5bbcc9ad5ee737f05a11fa3
Brayham Morales <moralesbrayham@gmail.com>
2024-10-23 18:12:04 -0600

    Convert queue to stack

--- MySweetProgram/MySweetProgram.java
+++ MySweetProgram/MySweetProgram.java
@@ -18,53 +18,7 @@ public class MySweetProgram{
         }
 
         in.close();
-        /*Scanner in = new Scanner(System.in);
-        System.out.println("Give us a size");
-        int size = in.nextInt();
         
-        int[] grades = new int[size];
-
-        System.out.println("Enter " + size + " numbers. Press enter after each");
-        for(int i = 0; i < size; i++){
-            grades[i] = in.nextInt();
-        }
-
-        for(int i = 0; i < size; i++){
-            System.out.println(grades[i]);
-        }
-
-        //String s = in.nextLine();
-
-        /*if(s.equals("Caleb")){
-            System.out.println("Hey Caleb");
-        }
-        else if(s.equals("Sally")){
-            System.out.println("Hey Sally");
-        }
-        else{
-            System.out.println("You're not welcome here");
-        }*/
-
-        //in.close();
-
-        //loops
-
-        /*int i = 0; //initialization
-        while (i < 10 ){ //condition
-            System.out.println("i is:" + i);
-            i++; //update
-        }*/
-
-       /*for(int i = 0; i < 10; i++){
-            System.out.println(i);
-        }*/
-
-        /*int i = 0;
-        do{
-            System.out.println(i);
-            i++;
-        }
-        while(i < 10);*/
 
     }
 }
